50d0e1aa24b9afb79c8fa70189ce576eb56b62a3
Bruno Krebs <bruno.krebs@auth0.com>
2020-05-26 19:39:50 -0300

    loading products on mount;

--- ui/src/features/products/ProductList.js
+++ ui/src/features/products/ProductList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Table } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, Route, useRouteMatch } from "react-router-dom";
@@ -28,22 +28,20 @@ function ProductList() {
   const products = useSelector((state) => state.productsSlice.products);
   const dispatch = useDispatch();
 
-  const loadProductsFromAPI = async () => {
-    try {
-      await dispatch(loadProducts());
-      console.log(`Fetched!`);
-    } catch (err) {
-      console.log(`Fetch failed!`);
-    }
-  }
+  useEffect(() => {
+    dispatch(loadProducts()).then(() => {
+      console.log("Lista de produtos carregada com sucesso.");
+    }).catch(err => {
+      console.error(err);
+    });
+  }, [dispatch]);
 
   return (
     <>
       <Route exact path={path}>
         <h1>Lista de Produtos</h1>
-        <Table dataSource={products} columns={columns} />
+        <Table rowKey="sku" dataSource={products} columns={columns} />
         <Link to="/produtos/novo">Novo Produto</Link>
-        <button onClick={loadProductsFromAPI}>load prods</button>
       </Route>
       <Route path={`${path}/:novo`}>
         <ProductForm />
