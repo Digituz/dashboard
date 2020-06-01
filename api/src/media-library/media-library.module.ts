20e40e5900dd40841bda0c405f0dd4b2f21fbf3c
Bruno Krebs <bruno.krebs@auth0.com>
2020-06-01 17:05:18 -0300

    configuring destination of uploaded files;

--- api/src/media-library/media-library.module.ts
+++ api/src/media-library/media-library.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MediaLibraryController } from './media-library.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
-  controllers: [MediaLibraryController]
+  controllers: [MediaLibraryController],
+  imports: [MulterModule.register({
+    dest: './uploaded-files',
+  })],
 })
 export class MediaLibraryModule {}
