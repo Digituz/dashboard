921373a1949a0359739b90f3ff30829deea6577c
Bruno Krebs <bruno.krebs@auth0.com>
2020-06-01 10:45:02 -0300

    starting to implement the upload feature;

--- ui/src/app/media-library/media-library.component.ts
+++ ui/src/app/media-library/media-library.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { UploadChangeParam } from 'ng-zorro-antd/upload/interface';
+import { BreadcrumbsService } from '@app/breadcrumbs/breadcrumbs.service';
 
 @Component({
   selector: 'app-media-library',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaLibraryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private msg: NzMessageService, private breadcrumbsService: BreadcrumbsService) {}
+
+  handleChange({ file, fileList }: UploadChangeParam): void {
+    const status = file.status;
+    if (status !== 'uploading') {
+      console.log(file, fileList);
+    }
+    if (status === 'done') {
+      this.msg.success(`${file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      this.msg.error(`${file.name} file upload failed.`);
+    }
+  }
 
   ngOnInit(): void {
+    this.breadcrumbsService.refreshBreadcrumbs([{ label: 'Imagens', url: '/imagens' }]);
   }
 
 }
--- ui/src/app/media-library/media-library.module.ts
+++ ui/src/app/media-library/media-library.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzUploadModule } from "ng-zorro-antd/upload";
 import { MediaLibraryComponent } from './media-library.component';
 import { MediaLibraryRoutingModule } from './media-library-routing.module';
+
+import {
+  PictureOutline,
+} from '@ant-design/icons-angular/icons';
+
+const icons: IconDefinition[] = [
+  PictureOutline,
+];
 
 @NgModule({
   declarations: [MediaLibraryComponent],
   imports: [
     CommonModule,
+    NzIconModule.forRoot(icons),
+    NzMessageModule,
+    NzUploadModule,
     MediaLibraryRoutingModule
   ]
 })
