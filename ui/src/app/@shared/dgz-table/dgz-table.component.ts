2f92e9095ddf6a8b69b05a4cc552c87da1f149ab
Bruno Krebs <bruno.krebs@auth0.com>
2020-06-12 10:16:56 -0300

    starting to define my own table component;

---
+++ ui/src/app/@shared/dgz-table/dgz-table.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dgz-table',
+  templateUrl: './dgz-table.component.html',
+  styleUrls: ['./dgz-table.component.scss']
+})
+export class DgzTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
--- ui/src/app/@shared/shared.module.ts
+++ ui/src/app/@shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { LoaderComponent } from './loader/loader.component';
 import { MediumEditorComponent } from './medium-editor/medium-editor.component';
+import { DgzTableComponent } from './dgz-table/dgz-table.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [LoaderComponent, MediumEditorComponent],
-  exports: [LoaderComponent, MediumEditorComponent],
+  declarations: [LoaderComponent, MediumEditorComponent, DgzTableComponent],
+  exports: [LoaderComponent, MediumEditorComponent, DgzTableComponent],
 })
 export class SharedModule {}
