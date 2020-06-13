01b102c550a8f31d769d1d5fce4a6b47ddee60cc
Bruno Krebs <bruno.krebs@auth0.com>
2020-06-13 18:40:50 -0300

    starting the filter impl;

--- ui/src/app/products/products-list/products-list.component.ts
+++ ui/src/app/products/products-list/products-list.component.ts
@@ -4,6 +4,7 @@ import { ProductsService } from '../products.service';
 import { DgzTableComponent } from '@app/@shared/dgz-table/dgz-table.component';
 import { IDataProvider, Pagination } from '@app/util/pagination';
 import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-products-list',
@@ -13,13 +14,23 @@ import { Observable } from 'rxjs';
 export class ProductsListComponent implements OnInit, IDataProvider<Product> {
   @ViewChild('productsTable') appFoo: DgzTableComponent<Product>;
   products: Product[];
+  formFields: FormGroup;
 
-  constructor(private productsService: ProductsService) {}
+  constructor(private productsService: ProductsService, private fb: FormBuilder) {}
 
   loadData(pageNumber: number, pageSize: number, sortedBy?: string, sortDirectionAscending?: boolean): Observable<Pagination<Product>> {
     return this.productsService.loadData(pageNumber, pageSize, sortedBy, sortDirectionAscending);
   }
 
   ngOnInit(): void {
+    this.formFields = this.fb.group({
+      query: '',
+      isActive: null,
+      withVariations: null,
+    });
+  }
+
+  queryProducts() {
+
   }
 }
