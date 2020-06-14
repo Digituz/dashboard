3efca7a3b63950d47e43511739b01ffc3e23e404
Bruno Krebs <bruno.krebs@auth0.com>
2020-06-14 04:38:08 -0300

    exporting dgz-sortable again;

--- ui/src/app/@shared/shared.module.ts
+++ ui/src/app/@shared/shared.module.ts
@@ -5,10 +5,11 @@ import { NzPaginationModule } from 'ng-zorro-antd/pagination';
 import { LoaderComponent } from './loader/loader.component';
 import { MediumEditorComponent } from './medium-editor/medium-editor.component';
 import { DgzTableComponent } from './dgz-table/dgz-table.component';
+import { DgzSortableDirective } from './dgz-table/dgz-sortable.directive';
 
 @NgModule({
   imports: [CommonModule, NzPaginationModule],
-  declarations: [LoaderComponent, MediumEditorComponent, DgzTableComponent],
-  exports: [LoaderComponent, MediumEditorComponent, DgzTableComponent],
+  declarations: [LoaderComponent, MediumEditorComponent, DgzTableComponent, DgzSortableDirective],
+  exports: [LoaderComponent, MediumEditorComponent, DgzTableComponent, DgzSortableDirective],
 })
 export class SharedModule {}
