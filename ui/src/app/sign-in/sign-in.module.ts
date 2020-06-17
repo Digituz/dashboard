eae71b2d1f63cd663dc1e5f6e1aa6de8e4c310da
Bruno Krebs <bruno.krebs@auth0.com>
2020-06-17 06:14:55 -0300

    some improvement on sign in screen;

--- ui/src/app/sign-in/sign-in.module.ts
+++ ui/src/app/sign-in/sign-in.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { SignInComponent } from './sign-in.component';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -9,6 +10,6 @@ import { SignInRoutingModule } from './sign-in-routing.module';
 
 @NgModule({
   declarations: [SignInComponent],
-  imports: [CommonModule, ReactiveFormsModule, ButtonModule, SignInRoutingModule],
+  imports: [CommonModule, ReactiveFormsModule, ButtonModule, InputTextModule, SignInRoutingModule],
 })
 export class SignInModule {}
