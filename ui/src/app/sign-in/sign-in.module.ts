1e1a2bff0a917ff67e8e091bd37d012dd9e76de8
Bruno Krebs <bruno.krebs@auth0.com>
2020-06-17 05:51:59 -0300

    starting up;

--- ui/src/app/sign-in/sign-in.module.ts
+++ ui/src/app/sign-in/sign-in.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ButtonModule } from 'primeng/button';
+
 import { SignInComponent } from './sign-in.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SignInRoutingModule } from './sign-in-routing.module';
 
 @NgModule({
   declarations: [SignInComponent],
-  imports: [
-    CommonModule,
-    ReactiveFormsModule,
-    SignInRoutingModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule, ButtonModule, SignInRoutingModule],
 })
 export class SignInModule {}
