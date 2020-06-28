b2fcaea2223d3411251529b0495acde3c386672c
Bruno Krebs <bruno.krebs@auth0.com>
2020-06-28 17:00:38 -0300

    uploading files;

--- bin/src/02-migrate-images-from-imagekit.js
+++ bin/src/02-migrate-images-from-imagekit.js
@@ -1,7 +1,9 @@
+const axios = require("axios");
 const fs = require("fs");
 const got = require("got");
 const os = require("os");
 const stream = require("stream");
+const FormData = require("form-data");
 const { promisify } = require("util");
 
 const pipeline = promisify(stream.pipeline);
@@ -47,26 +49,59 @@ async function downloadImage(imageId) {
     // 0. get access token
     const token = await getToken();
 
-    // 1. get list with all the products from Digituz
+    // 1. get list of skus (tags) from digituz
     const products = await getProducts(token);
+    const tags = products.map((product) => product.sku);
 
     // 2. get list of images (and their tags) from Frida Kahlo
     const taggedImages = await getTaggedImages();
 
-    // 3. upload images to Digituz while linking with tags
-    const uploadJobs = taggedImages.slice(0, 4).map((taggedImage, idx) => {
-      return new Promise((res, rej) => {
+    // 3. download images from imagekit and pass down info about path, image name, and tags
+    const downloadJobs = taggedImages.slice(0, 1).map((taggedImage, idx) => {
+      return new Promise((res) => {
         setTimeout(async () => {
-          console.log(`downloadind with ${idx * 400}ms delay`);
+          console.log(`downloading with ${idx * 400}ms delay`);
           const path = await downloadImage(taggedImage.imageId);
-          res(path);
+          res({
+            imageId: taggedImage.imageId,
+            path: path,
+            tags: taggedImage.tags,
+          });
         }, idx * 400);
       });
     });
 
-    Promise.all(uploadJobs).then((images) => {
-      console.log(images);
+    // 4. upload images to Digituz and link tags
+    const results = await Promise.all(downloadJobs);
+    const uploadJobs = results.map((downloadedImage, idx) => {
+      return new Promise((res, rej) => {
+        setTimeout(async () => {
+          console.log(downloadedImage.path);
+          const file = fs.createReadStream(downloadedImage.path);
+
+          const formData = new FormData();
+          formData.append("file", file);
+          await axios.post(
+            "http://localhost:3000/v1/media-library/upload",
+            formData,
+            {
+              headers: {
+                ...formData.getHeaders(),
+                Authorization: "Bearer " + token,
+              },
+            }
+          );
+          res();
+        }, idx * 400);
+      });
     });
+    Promise.all(uploadJobs)
+      .then(() => {
+        console.log("files uploaded");
+      })
+      .catch(() => {
+        console.log("something went wrong");
+      });
   } catch (error) {
     console.log(error);
   }
