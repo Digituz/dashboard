fd0efc408f3307c68aaea5974064af0b23e000c3
Bruno Krebs <bruno.krebs@auth0.com>
2020-06-30 18:25:45 -0300

    refactoring product images component;

--- ui/src/app/products/product-form/product-images/product-images.component.ts
+++ ui/src/app/products/product-form/product-images/product-images.component.ts
@@ -8,40 +8,40 @@ import { ImageService } from '@app/media-library/image.service';
   templateUrl: './product-images.component.html',
   styleUrls: ['./product-images.component.scss'],
 })
-export class ProductImagesComponent {
+export class ProductImagesComponent implements OnInit {
   @Input()
   product: Product;
   isModalVisible: boolean = false;
-  images: Image[] = [];
+  availableImages: Image[] = [];
   selectedImages: Image[] = [];
+  selectedImagesIds: number[] = [];
 
   @Output() imagesSelected = new EventEmitter<Image[]>();
 
   constructor(private imageService: ImageService) {}
 
+  ngOnInit(): void {
+    this.selectedImages = this.product.productImages.map((productImage) => productImage.image);
+    this.selectedImagesIds = this.selectedImages.map((selectedImage) => selectedImage.id);
+  }
+
   openImagesDialog() {
-    this.imageService.withTags(this.product.sku).subscribe((images) => {
-      this.images = images;
+    this.imageService.withTags(this.product.sku).subscribe((availableImages) => {
+      this.availableImages = availableImages;
+      this.selectedImages = this.availableImages.filter((availableImage) => {
+        return this.selectedImagesIds.includes(availableImage.id);
+      });
     });
     this.isModalVisible = true;
   }
 
   saveProductImages() {
-    const selectedImages = this.selectedImages;
-    this.product = {
-      ...this.product,
-      productImages: selectedImages.map((selectedImage, idx) => ({
-        order: idx,
-        image: selectedImage,
-      })),
-    };
+    this.imagesSelected.emit(this.selectedImages);
     this.closeDialog();
-    this.imagesSelected.emit(selectedImages);
   }
 
   closeDialog() {
     this.isModalVisible = false;
-    this.images = [];
-    this.selectedImages = [];
+    this.availableImages = [];
   }
 }
