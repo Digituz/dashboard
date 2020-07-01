15f5f9b96219ee53b736ef4dd625874113bbf9eb
Bruno Krebs <bruno.krebs@auth0.com>
2020-07-01 10:17:50 -0300

    i kinda love tests;

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,9 +1,5 @@
 import { ValidationPipe, LogLevel } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { config } from 'dotenv';
-
-// making sure we load env vars before any custom code
-config();
 
 import { AppModule } from './app.module';
 
--- api/test/app.e2e-spec.ts
+++ api/test/app.e2e-spec.ts
@@ -1,4 +1,9 @@
 import axios from 'axios';
+import { Client } from 'pg';
+import { config } from 'dotenv';
+
+// making sure we load env vars before any custom code
+config({ path: `${__dirname}/.env` })
 
 import { bootstrap } from '../src/server';
 import { INestApplication } from '@nestjs/common';
@@ -8,12 +13,25 @@ describe('AppController (e2e)', () => {
 
   beforeAll(async done => {
     const silentMode = true;
+    process.env.PGUSER=process.env.DATABASE_USER;
+    process.env.PGHOST=process.env.DATABASE_HOST;
+    process.env.PGPASSWORD=process.env.DATABASE_PASSWORD;
+    process.env.PGDATABASE=process.env.DATABASE_NAME;
     app = await bootstrap(silentMode);
     done();
   });
 
   afterAll(async done => {
+    const client = new Client();
+    await client.connect();
+    await client.query('delete from tag;');
+    await client.query('delete from product_image;');
+    await client.query('delete from product_variation;');
+    await client.query('delete from product;');
+    await client.end();
+
     await app.close();
+
     done();
   });
 
