a937cb0fd14c90361068ed4fb9d67712d5e68e2d
Bruno Krebs <bruno.krebs@auth0.com>
2020-07-03 00:02:50 -0300

    small amendment to reais pipe;

--- ui/src/app/@shared/pipes/reais.pipe.ts
+++ ui/src/app/@shared/pipes/reais.pipe.ts
@@ -5,6 +5,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ReaisPipe implements PipeTransform {
   transform(value: string): string {
-    return 'R$ ' + parseFloat(value).toFixed(2).toString().replace('.', ',');
+    return 'R$ ' + parseFloat(value || "0").toFixed(2).toString().replace('.', ',');
   }
 }
