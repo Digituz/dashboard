c6443814a5d0991fdf7d65bbc57f342a14228a2e
Bruno Krebs <bruno.krebs@auth0.com>
2020-07-14 17:09:59 -0300

    a more complete validaiton;

--- api/test/test-suites/sales-order/update-inventory/update-inventory.tests.ts
+++ api/test/test-suites/sales-order/update-inventory/update-inventory.tests.ts
@@ -5,7 +5,12 @@ import { insertProductFixtures } from '../../products/products-fixtures/products
 import saleOrderScenarios from '../sales-order.scenarios.json';
 import { SaleOrderDTO } from '../../../../src/sales-order/sale-order.dto';
 
-describe('update inventory when sale order is created or updated', () => {
+interface ItemPosition {
+  sku: string;
+  position: number;
+}
+
+describe('sale orders must update inventory', () => {
   let authorizedRequest: any;
 
   beforeAll(async () => {
@@ -28,7 +33,7 @@ describe('update inventory when sale order is created or updated', () => {
     expect(response.status).toBe(201);
   }
 
-  async function getCurrentPosition(sku: String) {
+  async function getCurrentPosition(sku: String): Promise<number> {
     const currrentPositionRows = await executeQuery(
       `select current_position
         from inventory i
@@ -38,11 +43,22 @@ describe('update inventory when sale order is created or updated', () => {
     return parseInt(currrentPositionRows[0].current_position);
   }
 
-  it.only('should update sale order payment status', async () => {
-    const saleOrder: SaleOrderDTO = saleOrderScenarios[0];
+  async function getCurrentPositions(items): Promise<ItemPosition[]> {
+    const getPositionJobs = items.map(item => {
+      return new Promise(async res => {
+        const initialPosition = await getCurrentPosition(item.sku);
+        res({
+          sku: item.sku,
+          position: initialPosition,
+        });
+      });
+    });
+    return Promise.all(getPositionJobs);
+  }
 
+  async function persistSaleOrder(saleOrder: SaleOrderDTO) {
     // get position before creating the sale order
-    const initialPosition = await getCurrentPosition(saleOrder.items[0].sku);
+    const initialPositions = await getCurrentPositions(saleOrder.items);
 
     // create the sale order
     const response = await axios.post(
@@ -55,51 +71,86 @@ describe('update inventory when sale order is created or updated', () => {
     expect(response.data).toBeDefined();
     expect(response.status).toBe(201);
 
-    const saleOrderReference = response.data.referenceCode;
+    return {
+      saleOrder: response.data,
+      positions: initialPositions,
+    };
+  }
 
-    // get position after creating the sale order
-    const positionAfterCreate = await getCurrentPosition(saleOrder.items[0].sku);
+  it('should subtract from inventory on creation', async () => {
+    const saleOrderDTO: SaleOrderDTO = saleOrderScenarios[0];
 
-    // validate that the position was properly update
-    expect(positionAfterCreate).toBe(initialPosition - saleOrder.items[0].amount)
-
-    // make some random change on the sale order
-    const updateResponse = await axios.post(
-      `http://localhost:3000/v1/sales-order/`,
-      {
-        ...saleOrder,
-        id: response.data.id,
-        customerName: 'John Doe'
-      },
-      authorizedRequest,
+    const { positions: initialPositions } = await persistSaleOrder(
+      saleOrderDTO,
     );
 
-    expect(updateResponse).toBeDefined();
-    expect(updateResponse.data).toBeDefined();
-    expect(updateResponse.status).toBe(201);
-    
-    // get position after updating the sale order
-    const positionAfterUpdate = await getCurrentPosition(saleOrder.items[0].sku);
+    // get position after creating the sale order
+    const positionsAfterCreating = await getCurrentPositions(
+      saleOrderDTO.items,
+    );
 
-    // validate that the position was not changed
-    expect(positionAfterUpdate).toBe(positionAfterCreate);
-
-    // change the sale order status
-    await changeOrderStatus(saleOrderReference, 'APPROVED');
-
-    // get position after changing the satus
-    const positionAfterStatusUpdate = await getCurrentPosition(saleOrder.items[0].sku);
-
-    // validate that the position was not changed
-    expect(positionAfterStatusUpdate).toBe(positionAfterCreate);
-
-    // cancel the sale order
-    await changeOrderStatus(saleOrderReference, 'CANCELLED');
-
-    // get position after changing the satus
-    const positionAfterCancelling = await getCurrentPosition(saleOrder.items[0].sku);
-
-    // validate that the position was not changed
-    expect(positionAfterCancelling).toBe(positionAfterCreate);
+    for (const positionAfterCreating of positionsAfterCreating) {
+      // validate that the position was properly update
+      const initialPosition = initialPositions.find(
+        position => position.sku === positionAfterCreating.sku,
+      );
+      const item = saleOrderDTO.items.find(
+        item => item.sku === positionAfterCreating.sku,
+      );
+      expect(positionAfterCreating.position).toBe(
+        initialPosition.position - item.amount,
+      );
+    }
   });
+
+  // it('should amend inventory on update', async () => {
+  //   const saleOrderDTO: SaleOrderDTO = saleOrderScenarios[0];
+
+  //   const { saleOrder, initialPosition } = await persistSaleOrder(saleOrderDTO);
+
+  //   // make some random change on the sale order
+  //   const updateResponse = await axios.post(
+  //     `http://localhost:3000/v1/sales-order/`,
+  //     {
+  //       ...saleOrder,
+  //       id: saleOrder.id,
+  //       customerName: 'John Doe',
+  //     },
+  //     authorizedRequest,
+  //   );
+
+  //   expect(updateResponse).toBeDefined();
+  //   expect(updateResponse.data).toBeDefined();
+  //   expect(updateResponse.status).toBe(201);
+
+  //   // get position after updating the sale order
+  //   const positionAfterUpdate = await getCurrentPosition(
+  //     saleOrder.items[0].sku,
+  //   );
+
+  //   // validate that the position was not changed
+  //   expect(positionAfterUpdate).toBe(positionAfterCreate);
+
+  //   // change the sale order status
+  //   await changeOrderStatus(saleOrderReference, 'APPROVED');
+
+  //   // get position after changing the status
+  //   const positionAfterStatusUpdate = await getCurrentPosition(
+  //     saleOrder.items[0].sku,
+  //   );
+
+  //   // validate that the position was not changed
+  //   expect(positionAfterStatusUpdate).toBe(positionAfterCreate);
+
+  //   // cancel the sale order
+  //   await changeOrderStatus(saleOrderReference, 'CANCELLED');
+
+  //   // get position after changing the status
+  //   const positionAfterCancelling = await getCurrentPosition(
+  //     saleOrder.items[0].sku,
+  //   );
+
+  //   // validate that the position was not changed
+  //   expect(positionAfterCancelling).toBe(initialPosition);
+  // });
 });
