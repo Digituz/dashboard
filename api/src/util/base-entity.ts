7e55d4787e42d9ac6d52080ad481c1e2df89e3a2
Bruno Krebs <bruno.krebs@auth0.com>
2020-07-17 11:01:54 -0300

    removing timestamp columns as it was causing issues;

--- api/src/util/base-entity.ts
+++ api/src/util/base-entity.ts
@@ -10,14 +10,5 @@ export abstract class BaseEntity {
   @PrimaryGeneratedColumn()
   id?: number;
 
-  @CreateDateColumn({ name: 'created_at', type: 'timestamp with time zone', nullable: false })
-  createdAt?: Date;
-
-  @UpdateDateColumn({ name: 'updated_at', type: 'timestamp with time zone', nullable: true })
-  updatedAt?: Date;
-
-  @DeleteDateColumn({ name: 'deleted_at', type: 'timestamp with time zone', nullable: true })
-  deletedAt?: Date;
-
   @VersionColumn() version?: number;
 }
