6f2384210f033658d9bf028d308c11ae2c1c3c5f
Bruno Krebs <bruno.krebs@auth0.com>
2020-07-22 19:41:41 -0300

    starting to use shipit;

--- README.md
+++ README.md
@@ -4,3 +4,10 @@ This project is composed by two subprojects: the `api` and the `ui`. You will fi
 
 - `api`: a Nest.js project
 - `ui`: an Angular project
+
+## Server Configuration
+
+1. [Initial Server Setup with Ubuntu 18.04](https://www.digitalocean.com/community/tutorials/initial-server-setup-with-ubuntu-18-04)
+2. [How To Install Nginx on Ubuntu 18.04](https://www.digitalocean.com/community/tutorials/how-to-install-nginx-on-ubuntu-18-04)
+3. [How To Secure Nginx with Let's Encrypt on Ubuntu 18.04](https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-18-04)
+4. [How To Install and Use PostgreSQL on Ubuntu 18.04](https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-18-04)
---
+++ deployment/shipitfile.js
@@ -0,0 +1,41 @@
+const deploy = require("shipit-deploy");
+
+module.exports = (shipit) => {
+  deploy(shipit);
+
+  shipit.initConfig({
+    default: {
+      workspace: "/tmp/dashboard",
+      deployTo: "/home/brunokrebs/dashboard",
+      repositoryUrl: "git@github.com:Digituz/dashboard.git",
+      ignores: [".git", "node_modules"],
+      keepReleases: 2,
+      keepWorkspace: false,
+      deleteOnRollback: false,
+      shallowClone: true,
+      deploy: {
+        remoteCopy: {
+          copyAsDir: false, // Should we copy as the dir (true) or the content of the dir (false)
+        },
+      },
+    },
+    production: {
+      servers: "brunokrebs@digituz.com.br",
+    },
+  });
+
+  shipit.blTask("digituz:build-api", async () => {
+    const commands = [
+      `cd ${shipit.releasePath}/api`,
+      "npm install --prooduction",
+      "npm run build",
+      "npx ts-node ./node_modules/typeorm/cli.js migration:run",
+      "npm run start:prod",
+    ];
+    await shipit.remote(commands.join(" && "));
+  });
+
+  shipit.on("published", function () {
+    shipit.start("digituz:build-api");
+  });
+};
