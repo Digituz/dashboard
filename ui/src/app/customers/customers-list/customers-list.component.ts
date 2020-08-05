0113c30dd82d8ffd344faba4040dfd7a65bf37ee
Bruno Krebs <bruno.krebs@auth0.com>
2020-08-05 06:44:29 -0300

    listing customers on the UI;

--- ui/src/app/customers/customers-list/customers-list.component.ts
+++ ui/src/app/customers/customers-list/customers-list.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Customer } from '../customer.entity';
+import { IDataProvider, QueryParam, Pagination } from '@app/util/pagination';
+import { DgzTableComponent } from '@app/@shared/dgz-table/dgz-table.component';
+import { BreadcrumbsService } from '@app/breadcrumbs/breadcrumbs.service';
+import { Observable } from 'rxjs';
+import { CustomersService } from '../customers.service';
 
 @Component({
   selector: 'app-customers-list',
   templateUrl: './customers-list.component.html',
-  styleUrls: ['./customers-list.component.scss']
+  styleUrls: ['./customers-list.component.scss'],
 })
-export class CustomersListComponent implements OnInit {
+export class CustomersListComponent implements OnInit, IDataProvider<Customer> {
+  @ViewChild('customersTable') salesOrdersTable: DgzTableComponent<Customer>;
+  queryParams: QueryParam[] = [];
+  query: string;
 
-  constructor() { }
+  constructor(private breadcrumbsService: BreadcrumbsService, private customersService: CustomersService) {}
 
   ngOnInit(): void {
+    this.breadcrumbsService.refreshBreadcrumbs('Clientes', [{ label: 'Clientes', url: '/customers' }]);
   }
 
+  loadData(
+    pageNumber: number,
+    pageSize: number,
+    sortedBy?: string,
+    sortDirectionAscending?: boolean
+  ): Observable<Pagination<Customer>> {
+    return this.customersService.loadData(pageNumber, pageSize, sortedBy, sortDirectionAscending, this.queryParams);
+  }
+
+  queryCustomers() {
+    this.queryParams = [{ key: 'query', value: this.query }];
+  }
 }
---
+++ ui/src/app/customers/customers.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { IDataProvider, Pagination } from '@app/util/pagination';
+import { Customer } from './customer.entity';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomersService implements IDataProvider<Customer> {
+  private CUSTOMERS_ENDPOINT = '/customers';
+
+  constructor(private httpClient: HttpClient) {}
+
+  loadData(
+    pageNumber: number,
+    pageSize: number,
+    sortedBy?: string,
+    sortDirectionAscending?: boolean,
+    queryParams?: import('../util/pagination').QueryParam[]
+  ): Observable<Pagination<Customer>> {
+    let query = `${this.CUSTOMERS_ENDPOINT}?page=${pageNumber}&limit=${pageSize}`;
+
+    if (sortedBy) {
+      query += `&sortedBy=${sortedBy}`;
+    }
+    if (sortDirectionAscending !== undefined) {
+      query += `&sortDirectionAscending=${sortDirectionAscending}`;
+    }
+
+    queryParams
+      .filter((queryParam) => {
+        return queryParam !== null && queryParam.value !== null && queryParam.value !== undefined;
+      })
+      .forEach((queryParam) => {
+        query += `&${queryParam.key}=${queryParam.value}`;
+      });
+
+    return this.httpClient.get<Pagination<Customer>>(query);
+  }
+}
