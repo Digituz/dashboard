e9b70a4820539f7f494d5da83ef4f81de7ad0619
Bruno Krebs <bruno.krebs@auth0.com>
2020-08-27 17:55:16 -0300

    adding filter cache;

---
+++ ui/src/app/filter-cache.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+export interface AnyObject {
+  [key: string]: any
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FilterCacheService {
+  constructor() {}
+
+  set(key: string, values: AnyObject) {
+    localStorage.setItem(key, JSON.stringify(values));
+  }
+
+  get(key: string): AnyObject {
+    return JSON.parse(localStorage.getItem(key));
+  }
+}
