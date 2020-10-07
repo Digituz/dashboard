e4bb9d05047f28555e24b2f4ade825b02d2a008b
Bruno Krebs <bruno.krebs@auth0.com>
2020-10-07 20:52:23 -0300

    starting up;

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -15,10 +15,28 @@ export class HomeComponent implements OnInit {
   @ViewChild('salesOrderTable') resultsTable: DgzTableComponent<SalesOrderDTO>;
   queryParams: QueryParam[] = [];
   query: string;
-
+  data: any;
   isLoading = false;
 
-  constructor(private homeService: HomeService, private breadcrumbsService: BreadcrumbsService) {}
+  constructor(private homeService: HomeService, private breadcrumbsService: BreadcrumbsService) {
+    this.data = {
+      labels: ['Quinta', 'Sexta', 'Sábado', 'Domingo', 'Segunda', 'Terça', 'Quarta'],
+      datasets: [
+        {
+          label: 'Boleto',
+          backgroundColor: '#42A5F5',
+          borderColor: '#1E88E5',
+          data: [65, 59, 80, 81, 56, 55, 40],
+        },
+        {
+          label: 'Cartão de Crédito',
+          backgroundColor: '#9CCC65',
+          borderColor: '#7CB342',
+          data: [28, 48, 40, 19, 86, 27, 90],
+        },
+      ],
+    };
+  }
 
   ngOnInit() {
     this.isLoading = false;
--- ui/src/app/home/home.module.ts
+++ ui/src/app/home/home.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
+import { ChartModule } from 'primeng/chart';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '@app/@shared';
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, HomeRoutingModule, SharedModule, ButtonModule],
+  imports: [CommonModule, TranslateModule, HomeRoutingModule, SharedModule, ButtonModule, ChartModule],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
