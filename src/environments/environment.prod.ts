c261fcc7598c15898cc08a3182403e858841958a
Bruno Krebs <bruno.krebs@auth0.com>
2020-10-12 21:14:20 -0300

    changing api url;

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,7 +10,7 @@ export const environment = {
   production: true,
   hmr: false,
   version: env.npm_package_version,
-  serverUrl: 'https://digituz.com.br/api/v1',
+  serverUrl: 'https://api.digituz.com.br/v1',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US', 'fr-FR'],
 };
