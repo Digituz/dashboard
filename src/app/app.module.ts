a4fa8cf691f17d387830b33b678af93dac339a2c
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2020-10-22 11:05:28 -0300

    rebase upstream e origin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ToastModule } from 'primeng/toast';
 import { MessagesComponent } from './messages/messages.component';
+import { SupplierComponent } from './supplier/supplier.component';
 
 @NgModule({
   imports: [
@@ -31,7 +32,7 @@ import { MessagesComponent } from './messages/messages.component';
     ShellModule,
     AppRoutingModule, // must be imported as the last module as it contains the fallback route
   ],
-  declarations: [AppComponent, MessagesComponent],
+  declarations: [AppComponent, MessagesComponent, SupplierComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/supplier/supplier.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SupplierComponent } from './supplier.component';
+
+@NgModule({
+  declarations: [SupplierComponent],
+  imports: [CommonModule],
+})
+export class SupplierModule {}
