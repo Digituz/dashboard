5cbbea08b2f5d916f204ec1b413d8ac6539992b9
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2020-10-25 21:49:59 -0300

    correção e validação da data

--- src/app/customers/customers-form/customers-form.component.ts
+++ src/app/customers/customers-form/customers-form.component.ts
@@ -3,7 +3,7 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 import { Customer } from '../customer.entity';
 import { CustomersService } from '../customers.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import { format, parse } from 'date-fns';
+import { format, isValid, isAfter, isBefore, addDays, parse } from 'date-fns';
 
 @Component({
   selector: 'app-customers-form',
@@ -14,6 +14,7 @@ export class CustomersFormComponent implements OnInit {
   formFields: FormGroup;
   customer: Customer;
   loading: boolean = true;
+  display: boolean = false;
 
   constructor(
     private fb: FormBuilder,
@@ -39,8 +40,9 @@ export class CustomersFormComponent implements OnInit {
   private configureFormFields(customer: Customer) {
     let birthDay: string;
     if (customer.birthday) {
-      const customerBirthDay = customer.birthday.toString().split('-');
-      birthDay = `${customerBirthDay[2]}/${customerBirthDay[1]}/${customerBirthDay[0]}`;
+      //estou fazendo a adição de 1 dia pois por algum motivo estava aparecendo
+      //a data com 1 dia subitraido ex: no banco 08/06 no form 07/06
+      birthDay = format(addDays(new Date(customer.birthday), 1), 'dd/MM/yyyy');
     }
     this.formFields = this.fb.group({
       cpf: [customer.cpf || ''],
@@ -58,19 +60,44 @@ export class CustomersFormComponent implements OnInit {
     });
     this.loading = false;
   }
-
+  showDialog() {
+    this.display = true;
+  }
   submitCustomer() {
     const customer = {
       ...this.formFields.value,
       id: this.customer.id,
     };
 
-    const birthDayformatToDatabase = customer.birthday.split('/');
-    customer.birthday = `${birthDayformatToDatabase[2]}-${birthDayformatToDatabase[1]}-${birthDayformatToDatabase[0]}`;
-    //console.log(parse('08/06/1198', 'yyyy-MM-dd', new Date()));
-    //console.log(customer.birthday);
+    customer.birthday = parse(customer.birthday, 'dd/MM/yyyy', new Date());
+
+    if (!this.validateDate(customer.birthday)) {
+      this.showDialog();
+      return;
+    }
+
+    customer.birthday = format(customer.birthday, 'yyyy-MM-dd');
+
     this.customersService.saveCustomer(customer).subscribe(() => {
       this.router.navigate(['/customers']);
     });
+  }
+
+  validateDate(date: string) {
+    const dataEmValidacao = new Date(date);
+    if (isValid(dataEmValidacao)) {
+      if (isAfter(dataEmValidacao, new Date())) {
+        this.display = false;
+        return false;
+      } else if (isBefore(dataEmValidacao, new Date('01/01/1990'))) {
+        this.display = false;
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      this.display = false;
+      return false;
+    }
   }
 }
