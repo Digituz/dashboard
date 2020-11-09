fb1f87d58eecc6d5d0301d303cccb11666004c1a
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2020-11-09 11:52:38 -0300

    formulario basico

--- src/app/purchase-orders/purchase-orders-form/purchase-orders-form.component.ts
+++ src/app/purchase-orders/purchase-orders-form/purchase-orders-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Supplier } from '@app/supplier/supplier.entity';
+import { SupplierService } from '@app/supplier/supplier.service';
+import { PurchaseOrder } from '../purchase-order.entity';
 
 @Component({
   selector: 'app-purchase-orders-form',
@@ -6,7 +11,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./purchase-orders-form.component.scss'],
 })
 export class PurchaseOrdersFormComponent implements OnInit {
-  constructor() {}
+  purchaseOrder: PurchaseOrder;
+  formFields: FormGroup;
+  loading: boolean = true;
+  constructor(private fb: FormBuilder, private router: Router, private route: ActivatedRoute) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    const id = this.route.snapshot.params.referenceCode;
+
+    if (id === 'new') {
+      this.purchaseOrder = {};
+      this.configureFormFields(this.purchaseOrder);
+    } else {
+    }
+  }
+
+  configureFormFields(purchaseOrder: PurchaseOrder) {
+    this.formFields = this.fb.group({
+      referenceCode: [purchaseOrder.referenceCode || ''],
+      creationDate: [purchaseOrder.creationDate || ''],
+      total: [purchaseOrder.total || ''],
+      discount: [purchaseOrder.discount || ''],
+      shippingPrice: [purchaseOrder.shippingPrice || ''],
+      items: [purchaseOrder.items || ''],
+    });
+    this.loading = false;
+  }
+
+  submit() {
+    console.log(this.formFields.value);
+    return;
+  }
 }
