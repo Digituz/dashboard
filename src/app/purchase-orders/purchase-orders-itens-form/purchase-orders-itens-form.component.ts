27184cdc39ef5ac982c3e2f4caa4de29c1cd3905
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2020-11-09 12:11:10 -0300

    criado formulario de itens

---
+++ src/app/purchase-orders/purchase-orders-itens-form/purchase-orders-itens-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-orders-itens-form',
+  templateUrl: './purchase-orders-itens-form.component.html',
+  styleUrls: ['./purchase-orders-itens-form.component.scss'],
+})
+export class PurchaseOrdersItensFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
