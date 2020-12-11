e7c403a6235fdc57e50bf910f042c4230e430212
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2020-12-11 08:29:36 -0300

    layout lista de produtos

--- src/app/mercado-livre/mercado-livre.module.ts
+++ src/app/mercado-livre/mercado-livre.module.ts
@@ -4,9 +4,21 @@ import { MercadoLivreComponent } from './mercado-livre.component';
 import { MercadoLivreRoutingModule } from './mercado-livre-routing.module';
 import { ButtonModule } from 'primeng/button';
 import { MlProductListComponent } from './ml-product-list/ml-product-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { SharedModule } from '@app/@shared';
 
 @NgModule({
   declarations: [MercadoLivreComponent, MlProductListComponent],
-  imports: [CommonModule, MercadoLivreRoutingModule, ButtonModule],
+  imports: [
+    CommonModule,
+    MercadoLivreRoutingModule,
+    ButtonModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule,
+  ],
 })
 export class MercadoLivreModule {}
--- src/app/mercado-livre/ml-product-list/ml-product-list.component.ts
+++ src/app/mercado-livre/ml-product-list/ml-product-list.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { DgzTableComponent } from '@app/@shared/dgz-table/dgz-table.component';
+import Product from '@app/products/product.entity';
+import { ProductsService } from '@app/products/products.service';
+import { Pagination, QueryParam } from '@app/util/pagination';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-ml-product-list',
@@ -7,9 +11,34 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./ml-product-list.component.scss'],
 })
 export class MlProductListComponent implements OnInit {
-  constructor(private route: ActivatedRoute) {
-    console.log(this.route.snapshot.queryParams.token);
-  }
+  @ViewChild('MLProductsTable') resultsTable: DgzTableComponent<any>;
+  queryParams: QueryParam[] = [];
+  query: string;
+  constructor(private productService: ProductsService) {}
 
   ngOnInit(): void {}
+
+  loadData(
+    pageNumber: number,
+    pageSize: number,
+    sortedBy?: string,
+    sortDirectionAscending?: boolean,
+    queryParams?: QueryParam[]
+  ): Observable<Pagination<Product>> {
+    return this.productService.loadData(pageNumber, pageSize, sortedBy, sortDirectionAscending, queryParams);
+  }
+
+  queryCustomers() {
+    this.queryParams = [{ key: 'query', value: this.query }];
+    this.resultsTable.reload(this.queryParams);
+  }
+
+  updateQueryParams(queryParams: QueryParam[]) {
+    this.query = queryParams.find((q) => q.key === 'query')?.value.toString();
+  }
+
+  resetFilter() {
+    this.query = '';
+    return localStorage.removeItem('mercado-livre-list');
+  }
 }
