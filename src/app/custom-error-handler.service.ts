381a66b2ad0686f2eabdc4c36307c7c31a2abdfb
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2021-04-07 14:19:38 -0300

    logando erros do dashboardo

--- src/app/custom-error-handler.service.ts
+++ src/app/custom-error-handler.service.ts
@@ -4,11 +4,33 @@ import { environment } from '@env/environment';
 export class CustomErrorHandler implements ErrorHandler {
   handleError(error: any) {
     console.error(error);
-    console.log('aqui');
-    const message = 'Ouve um erro inesperado, para mais informações acesse o datadog';
+
     const url = environment.production
       ? 'https://hooks.slack.com/services/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'
       : 'https://hooks.slack.com/services/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';
+
+    const message = {
+      text: 'Falha no DashBoard',
+      attachments: [
+        {
+          text: 'Houve um erro inesperado',
+          fallback: 'Não a erro',
+          callback_id: 'wopr_game',
+          color: '#3AA3E3',
+          attachment_type: 'default',
+          actions: [
+            {
+              name: 'goDatadog',
+              text: 'Ir para os logs do datadog',
+              style: 'danger',
+              type: 'button',
+              value: 'war',
+              url: 'https://app.datadoghq.com/logs?index=%2A&query=',
+            },
+          ],
+        },
+      ],
+    };
 
     fetch(url, {
       mode: 'no-cors',
@@ -18,7 +40,7 @@ export class CustomErrorHandler implements ErrorHandler {
         'Content-Type': 'application/x-www-form-urlencoded',
         'Access-Control-Allow-Origin': '*',
       },
-      body: JSON.stringify({ text: message }),
+      body: JSON.stringify(message),
     })
       .then(() => console.log('sucesso'))
       .catch((err) => console.log(err));
