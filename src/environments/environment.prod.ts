da4628488631c72151d2ba2d966bec6ddbbb811d
Patrick da Silva Nicezi <patrickk0806@hotmail.com>
2021-04-08 07:33:46 -0300

    removendo alterações

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,9 +4,9 @@
 // This is useful for granularity you might need beyond just the environment.
 // Note that as usual, any environment variables you expose through it will end up in your
 // bundle, and you should not use it for any sensitive information like passwords or keys.
-import { env, Environment } from './.env';
+import { env } from './.env';
 
-export const environment: Environment = {
+export const environment = {
   production: true,
   hmr: false,
   version: env.npm_package_version,
